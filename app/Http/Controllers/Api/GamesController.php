86200175c0f8c837437ebd2b477da483db697e86
almoatasembellah <moatasembellah80@gmail.com>
2023-06-24 01:33:54 +0300

    modify

--- app/Http/Controllers/Api/GamesController.php
+++ app/Http/Controllers/Api/GamesController.php
@@ -36,7 +36,6 @@ class GamesController extends Controller
         $games = Game::where('user_id' , '!=' , $request->user()->id)
             ->where('city_id' , $request->get('city_id'))
             ->where('date', '>=', Carbon::today()->format('Y-m-d'))
-            ->where('time' , '>=' , Carbon::now()->addHours(2)->format('h:i A'))
             ->where('type' , GameTypes::COMPETITIVE)
             ->where(function ($query) use ($request) {
                 if ($request->get('area_id')){
